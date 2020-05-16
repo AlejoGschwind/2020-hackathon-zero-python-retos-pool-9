b2181358d829717ee4b0d8f905d57f98ba6991a0
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-05-16 12:30:50 -0300

    Kata2

--- src/kata2/rpg.py
+++ src/kata2/rpg.py
@@ -4,10 +4,12 @@ import random
 import string
 
 def RandomPasswordGenerator(passLen=10):
-    #
-    #
-    
-    #
-    #
+    # Valores validos para una contraseña
+    valores = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ<=>@#%&+"
 
-    return ""
+    password = ""
+
+    # Agregamos caracteres aletorios, a partir de valores.
+    password = password.join([random.choice(valores) for i in range(passLen)])
+
+    return password
