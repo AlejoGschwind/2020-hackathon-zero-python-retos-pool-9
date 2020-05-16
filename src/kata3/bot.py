109d036f39b2e7a5490587c54a0ea23b162efdaf
Alejo Gschwind <alejogschwind.97@gmail.com>
2020-05-16 13:04:17 -0300

    Kata3

--- src/kata3/bot.py
+++ src/kata3/bot.py
@@ -12,20 +12,31 @@ logger = logging.getLogger(__name__)
 # Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
 def start(update, context):
     """Envia un mensaje cuando se emita el comando /start."""
-    return ""
+    update.message.reply_text('Hola, Geeks!')
 
 def help(update, context):
     """Envia un mensaje cuando se emita el comando /help."""
-    return ""
+    update.message.reply_text('Ayudame!')
 
 def mayus(update, context):
-        #
-        return ""
+        # Mensaje a partir del areglo de argumentos.
+        msj = ""
+        for i in context.args:
+            msj += context.args[i] + ' '
+        
+        # Pasamos el mensaje a mayusculas
+        msj_mayusculas = msj.upper()
+        return msj_mayusculas
 
 def alreves(update, context):
     """Repite el mensaje del usuario."""
-    #
-    return ""
+    msj = ""
+        for i in context.args:
+            msj += context.args[i] + ' '
+    
+    # Mensaje al reves
+    msj_alreves = msj[::-1]
+    return msj_alreves
 
 def error(update, context):
     """Envia los errores por consola"""
@@ -37,15 +48,20 @@ def main():
     updater = Updater("", use_context=True)
 
     # Es el Registro de Comandos a través del dispartcher
-    dp = #
+    dp = updater.dispatcher
 
     # Añadimos a la lista de Registro todos los comandos con su función [start - help - mayus]
-    #
-    #
-    #
+    # Añadiendo un manejador al comando /start
+    dp.add_handler(CommandHandler("start", start))
+    
+    # Añadiendo un manejador al comando /help
+    dp.add_handler(CommandHandler("help", help))
+
+    # Añadiendo un manejador al comando /mayus
+    dp.add_handler(CommandHandler("mayus", mayus))
 
     # Este comando es un Trigger que se lanza cuando no hay comandos [alreves]
-    #
+    dp.add_handler(MessageHandler(Filters.text, alreves))
     
     # Y este espera al error
     dp.add_error_handler(error)
